56b1091d0489d31d31a0a9daa24e2e2546c342e7
sharad8979 <sharadsharma8439@gmail.com>
2024-09-27 11:54:10 +0530

    Add files via upload

---
+++ SIZE.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+      printf("Size of Int : %u " , sizeof(int));
+      printf("\n Size of Float : %u",sizeof(float));
+      printf("\n Size of char : %u ",sizeof(char));
+
+     getch();
+	}
